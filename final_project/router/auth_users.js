8b4878cfad942c1695d53f17f7b01bf4a3c8a3f4
Zahdan Wahid Arohim <zahdan213@gmail.com>
2026-04-09 01:38:15 +0000

    modified some code

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -13,22 +13,6 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
 }
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-// Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
-module.exports.users = users;
-
 
 // Task 8: Login
 regd_users.post("/login", (req,res) => {
